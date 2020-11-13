c92670abfa4fb3643d5f078ebdde45100c96c916
Daniel Hui <danielhui@Daniels-Air.fios-router.home>
2020-11-12 22:38:58 -0500

    beta

--- js/index.js
+++ js/index.js
@@ -1,6 +1,8 @@
 
 
 $(document).ready(function() {
+
+
 	var modal = document.getElementById("imgModal");
 
 	//var img = document.getElementById("myImg");
@@ -11,9 +13,16 @@ $(document).ready(function() {
 	for(var a = 0; a < img.length; a++) {
 	    img[a].addEventListener("click", function(el) {
 	          modal.style.display = "block";
-			  modalImg.src = this.src;
+
+	          var src=this.src.substring(
+			    this.src.lastIndexOf("/") + 1, 
+			    this.src.lastIndexOf(".")
+			  );
+
+			  console.log()
+
+			  modalImg.src = "images/full-resolution/" + src + ".jpg";
 			  captionText.innerHTML = this.alt;
-			  console.log("cliekd");
 	    }, false)
 	}
 
@@ -24,5 +33,12 @@ $(document).ready(function() {
 	// When the user clicks on <span> (x), close the modal
 	span.onclick = function() { 
 	  modal.style.display = "none";
+	}
+
+	// if modal is open and clicked on then close it
+	modal.onclick = function() {
+	  if(this.style.display == "block") {
+	  	this.style.display = "none";
+	  }	
 	}
 });
